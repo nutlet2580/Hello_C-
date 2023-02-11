a2f8027752b16e07463ffcd93b73838a0cee9869
Anastasiia Mukhina <nutlet2580@gmail.com>
2023-02-11 11:49:10 +0900

    В коде ошибка, не забудь исправить

---
+++ Examples/If_Else/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите ваше имя: ");
+string? username = Console.ReadLine();
+
+if(username == "Маша")
+
+{
+    Console.WriteLine("Мария, здравствуйте!!");
+}
+
+else
+
+{
+    Console.Write("Привет,");
+    Console.WriteLine(username);
+
+}
